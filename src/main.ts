7a4caca18eb12ac2f859beb945a31bb5010f1f42
vsmm-world <thekingjarvis792@gmail.com>
2023-12-23 23:06:43 +0530

    setting up swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Creating API in NestJS')
+    .setDescription('Learning While Creating API in NestJS')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
